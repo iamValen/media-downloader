e8868d4dfe6b170fd64c4bfa344a779f1c9dd99e
iamValen <valentimk04@gmail.com>
2025-10-09 10:12:28 +0100

    Host downloads completed

--- config.py
+++ config.py
@@ -2,7 +2,10 @@ import os
 from pathlib import Path
 
 class Config:
+    # Host
     NETWORK_DOWNLOAD_PATH = os.environ.get('NETWORK_DOWNLOAD_PATH', '/home/vln/Desktop/test')
+    
+    # Device location - in porgress
     TEMP_DOWNLOAD_PATH = os.environ.get('TEMP_DOWNLOAD_PATH', './downloads/temp')
 
     Path(NETWORK_DOWNLOAD_PATH).mkdir(parents=True, exist_ok=True)
